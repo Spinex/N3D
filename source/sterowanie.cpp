f31f7985b6c921208e0caa3ae2bf6acabbe35737
Spinex <sebamm@wp.pl>
2013-01-12 11:21:33 +0100

    Próby ręcznej naprawy kodu.

--- source/sterowanie.cpp
+++ source/sterowanie.cpp
@@ -33,7 +33,6 @@ void IrrlichtInternals::sterowaniePostacia() {
 	if (bStany[2]) str = "forward: true";  
 	else str = "forward: false";
 		konsola->setMessage(2, str);
-<<<<<<< HEAD
 
 	if (bStany[3]) str = "back: true";  
 	else str = "back: false";
@@ -46,54 +45,8 @@ void IrrlichtInternals::sterowaniePostacia() {
 	if (bStany[5]) str = "down: true";  
 	else str = "down: false";
 		konsola->setMessage(5, str);
-
-	// *************************************************************************************************************    
-
-
-	//Roboczo na szybko, pozniej zaimplementuje sie skakanie do metody Move() 
-	// Odtad **********************************************************************************************************
-
-	if(!bStany[5])
-    {
-         bohaterGry->fallDown();
-         bohaterGry->getZnacznikiKolizji().setPosition(bohaterGry->getPosition(), bohaterGry->getRotation(), bohaterGry->getDirection());   
-         bohaterGry->getZnacznikiKolizji().setRotation(bohaterGry->getRotation(), bohaterGry->getDirection());
-    }     
-    else
-    {   
-         bohaterGry->stopFallingDown();  
-         bohaterGry->getZnacznikiKolizji().setPosition(bohaterGry->getPosition(), bohaterGry->getRotation(), bohaterGry->getDirection());   
-         bohaterGry->getZnacznikiKolizji().setRotation(bohaterGry->getRotation(), bohaterGry->getDirection());  
-    }      
-      
-	if ( aktywne.IsKeyDown( klawiszSkoku ) && !bohaterGry->getJumpState() && bStany[5] )  
-    { 
-         bohaterGry->setJumpState(true);
-         bohaterGry->stopFallingDown();  
-    }     
-	else if (bStany[5] || bStany[4]) bohaterGry->setJumpState(false);
-	
-	if (bohaterGry->getJumpState())
-    {
-		bohaterGry->jump();
-		moveCameraControl(false);
-	
-	}
-
-=======
-
-	if (bStany[3]) str = "back: true";  
-	else str = "back: false";
-		konsola->setMessage(3, str);
-
-	if (bStany[4]) str = "up: true";  
-	else str = "up: false";
-		konsola->setMessage(4, str);
-
-	if (bStany[5]) str = "down: true";  
-	else str = "down: false";
-		konsola->setMessage(5, str);
-
+		
+		
        if(!bStany[5])
        {
          bohaterGry->fallDown();
@@ -106,21 +59,24 @@ void IrrlichtInternals::sterowaniePostacia() {
          bohaterGry->getZnacznikiKolizji().setPosition(bohaterGry->getPosition(), bohaterGry->getRotation(), bohaterGry->getDirection());   
          bohaterGry->getZnacznikiKolizji().setRotation(bohaterGry->getRotation(), bohaterGry->getDirection());  
        }      
-        
+          	   
     
-       if ( aktywne.IsKeyDown( klawiszSkoku ) && !bohaterGry->getJumpState() && bStany[5])  
+       if ( aktywne.IsKeyDown( klawiszSkoku ) && !bohaterGry->getJumpState() && bStany[5] ) 	        
        { 
          bohaterGry->setJumpState(true);
-         bohaterGry->stopFallingDown();  
-       }     
-       else if (bStany[5] || bStany[4]) bohaterGry->setJumpState(false);	
-       if (bohaterGry->getJumpState())
-       {
-	 bohaterGry->jump();
-	 moveCameraControl(false);
+	 bohaterGry->stopFallingDown();  
        }
+       else if	(aktywne.IsKeyDown( klawiszSkoku ) && (!bStany[5] && bStany[2]))
+       {
+         bohaterGry->setJumpState(true);
+	 bohaterGry->stopFallingDown();  
+	 bohaterGry->rotate(bohaterGry->getRotation() + 180);
+       }	   
+       else if (bStany[5] || bStany[4]) bohaterGry->setJumpState(false);	
+       if (bohaterGry->getJumpState()) bohaterGry->jump();
+	   
+	   	     
 	 
->>>>>>> 29bc7c1274edef6c285aa13df94682ddfd7b024d
 	int player_direction = 0;
 	if ( aktywne.IsKeyDown( irr::KEY_KEY_W ) )
 		player_direction |= FORWARD;
@@ -153,6 +109,9 @@ void IrrlichtInternals::sterowaniePostacia() {
 		bohaterGry->getZnacznikiKolizji().setRotation(bohaterGry->getRotation(), bohaterGry->getDirection());
 
 		bohaterGry->move(WALK, bohaterGry->getDirection());
+		
+		if (!bohaterGry->getJumpState())
 		moveCameraControl(false);
+		else moveCameraControl(true);
 	}
 }
